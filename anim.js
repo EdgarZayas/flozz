3dc6c1ed9b8454b69456895965fe511ee8ac993a
EdgarZayas <zayasedgar6@gmail.com>
2026-09-22 02:06:06 -0600

    Cambioddscssss

--- anim.js
+++ anim.js
@@ -4,32 +4,19 @@ var lyrics = document.querySelector("#lyrics");
 
 // Letras y tiempos aproximados
 var lyricsData = [
-  { text: "It might not be the right time", time: 15 },
-  { text: "I might not be the right one", time: 23 },
+  { text: "It might not be the right time", time: 75 },
+  { text: "I might not be the right one", time: 80 },
 
-  { text: "But there's something about us I want to say", time: 30 },
-  { text: "'Cause there's something between us anyway", time: 38 },
+  { text: "But there's something about us I want to say", time: 85 },
+  { text: "'Cause there's something between us anyway", time: 90 },
 
-  { text: "I might not be the right one", time: 45 },
-  { text: "It might not be the right time", time: 53 },
+  { text: "I might not be the right one", time: 96 },
+  { text: "It might not be the right time", time: 100 },
 
-  { text: "But there's something about us I've got to do", time: 60 },
-  { text: "Some kind of secret I will share with you", time: 68 },
+  { text: "But there's something about us I've got to do", time:105 },
+  { text: "Some kind of secret I will share with you", time: 110 },
 
-  // Interludio
-  { text: "", time: 75 },
 
-  { text: "It might not be the right time", time: 105 },
-  { text: "I might not be the right one", time: 113 },
-
-  { text: "But there's something about us I want to say", time: 120 },
-  { text: "'Cause there's something between us anyway", time: 128 },
-
-  // Puente instrumental
-  { text: "", time: 135 },
-
-  // Outro
-  { text: "", time: 165 }
 ];
 
 
